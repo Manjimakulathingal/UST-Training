d5153339c0e58412511e4ef00d1aeffbba1619d7
Manjimakulathingal <129931081+Manjimakulathingal@users.noreply.github.com>
2023-04-14 14:52:29 +0530

    Add files via upload

---
+++ sorting/sorting/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your  arraysize: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine("enter the array: ");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int temp;
+            for (int i = 0; i < size - 1; i++)
+            {
+                for (int j = 0; j < size - i - 1; j++)
+                {
+                    if (arr[j] < arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("sorted array: ");
+            for(int i = 0; i < size; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
+           
+
+
