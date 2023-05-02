1c7c2b342a06eb1b4ee8b5f3c458a4952e1787d8
Manjimakulathingal <129931081+Manjimakulathingal@users.noreply.github.com>
2023-05-02 17:18:18 +0530

    Add files via upload

---
+++ DAY 17/suminarray/suminarray/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suminarray
+{
+ 
+public class HelloWorld
+        {
+
+            public static void subArraySum(int[] arr, int n, int sum)
+            {
+
+                
+                for (int i = 0; i < n; i++)
+                {
+                    int currentSum = arr[i];
+
+                    if (currentSum == sum)
+                    {
+                        Console.WriteLine("Sum found at indexe " + i);
+                        return;
+                    }
+                    else
+                    {
+
+                        
+                        for (int j = i + 1; j < n; j++)
+                        {
+                            currentSum += arr[j];
+
+                            if (currentSum == sum)
+                            {
+                                Console.WriteLine("Sum found between indexes " + i + " and " + j);
+                                return;
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine("No subarray found");
+                return;
+            }
+
+            public static void Main(string[] args)
+            {
+                int[] arr = { 15, 2, 4, 8, 9, 5, 10, 23 };
+                int n = arr.Length;
+                int sum = 23;
+                subArraySum(arr, n, sum);
+            }
+        }
+
+       
+
+    }
+
+
+           
+        
+    
