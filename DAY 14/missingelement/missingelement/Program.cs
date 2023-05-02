78c86fa44b604cd25638748e55665bde0177edc5
Manjimakulathingal <129931081+Manjimakulathingal@users.noreply.github.com>
2023-05-02 17:08:07 +0530

    Add files via upload

---
+++ DAY 14/missingelement/missingelement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace missingelement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100];
+            int total = 0;
+            int element;
+            Console.WriteLine("enter the size:");
+            int n = int.Parse(Console.ReadLine());
+            int sum = n * (n + 1) / 2;
+
+            Console.WriteLine("enter the elements:");
+            for(int i = 0; i <n-1; i++) {
+                arr[i] = int.Parse(Console.ReadLine());
+                total += arr[i];
+            
+            }
+
+            element = sum - total;
+            Console.WriteLine(element);
+        }
+    }
+}
---
+++ DAY 14/multicastdelegate/multicastdelegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multicastdelegate
+{
+    internal class Program
+    {
+        static void Add(int i, int j)
+        {
+            Console.WriteLine("sum: " + (i + j));
+        }
+        static void Sub(int i, int j)
+        {
+            Console.WriteLine("sub: " + (i - j));
+        }
+
+        public delegate void refto(int i, int j);
+        static void Main(string[] args)
+        {
+
+            refto multicast = Add;
+            multicast += Sub;
+            multicast(20, 10);
+        }
+    }
+}
