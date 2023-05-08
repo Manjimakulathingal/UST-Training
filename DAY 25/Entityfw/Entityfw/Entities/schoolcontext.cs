1d7ffdebd94f647d87d40222d4dd2f79aa390762
Manjimakulathingal <129931081+Manjimakulathingal@users.noreply.github.com>
2023-05-08 09:36:41 +0530

    Add files via upload

---
+++ DAY 25/Entityfw/Entityfw/Entities/schoolcontext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entityfw.Entities
+{
+    public class schoolcontext : DbContext
+    {
+        public schoolcontext() : base()
+        { 
+
+        }
+        public DbSet<student> students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+    }
+}
---
+++ DAY 25/Entityfw/Entityfw/Program.cs
@@ -0,0 +1,23 @@
+﻿using Entityfw.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entityfw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new schoolcontext())
+            {
+                var stud = new student() { StudentName = "Bill" };
+
+                ctx.students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
