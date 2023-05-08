2acb5ae9dfabc32a1c548d091f143fb2f49fecc9
Manjimakulathingal <129931081+Manjimakulathingal@users.noreply.github.com>
2023-05-08 09:39:28 +0530

    Add files via upload

---
+++ DAY 24/EMPDB/EMPDB/Program.cs
@@ -0,0 +1,71 @@
+﻿using EMPDB;
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace EFDemo1
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+
+           EmpEntities emp = new EmpEntities();
+
+            foreach (var item in emp.Depts)
+
+            {
+
+                Console.WriteLine(item.DeptName);
+
+            }
+
+           /* using (var context = new EmpEntities())
+
+            {
+
+                var Dept = new Dept()
+
+                {
+
+                    DeptId = 110,
+
+                    DeptName = "IT"
+
+                };
+
+                context.Depts.Add(Dept);
+
+                context.SaveChanges();
+            }
+
+            using (var context = new EmpEntities())
+            {
+                var dept = context.Depts.First<Dept>();
+                dept.DeptName = "kulam";
+                context.SaveChanges();
+            }
+            using (var context = new EmpEntities())
+            {
+                var dept = context.Depts.First<Dept>();
+                context.Depts.Remove(dept);
+                context.SaveChanges();
+
+            }*/
+
+           
+        }
+    }
+}
+
+
---
+++ DAY 24/SQLQuery1.sql
@@ -0,0 +1,8 @@
+
+Create table Emp
+
+(EmpId int primary key not null,
+
+EmpName varchar(30) not null,
+
+DeptId int foreign key references Depart(DeptId));
