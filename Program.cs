c16bd23d82395edce0c7a0b238ab2df4219ce024
Manjimakulathingal <129931081+Manjimakulathingal@users.noreply.github.com>
2023-05-17 09:27:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Method1();
+        Method2();
+        Method3();
+        Console.ReadKey();
+    }
+
+    public static async Task Method1()
+    {
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(" ozo");
+                // Do something
+                Task.Delay(100).Wait();
+            }
+        });
+    }
+
+
+    public static void Method2()
+    {
+        for (int i = 0; i < 25; i++)
+        {
+            Console.WriteLine(" ooz");
+            // Do something
+            Task.Delay(100).Wait();
+        }
+    }
+
+    public static void Method3()
+    {
+        for (int i = 0; i < 25; i++)
+        {
+            Console.WriteLine(" zoo");
+            // Do something
+            Task.Delay(100).Wait();
+        }
+    }
+}
